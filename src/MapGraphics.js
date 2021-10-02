bb11d88ef65135f6410aa91d3701965b6214b489
Omar Ismail <omariu_dbpro@outlook.com>
2021-10-02 13:36:14 +0300

    GraphicsLayer

--- src/MapGraphics.js
+++ src/MapGraphics.js
@@ -1,22 +1,78 @@
 import React, { useEffect } from "react";
 import Graphic from "@arcgis/core/Graphic";
 
-const MapGraphics = ({ view }) => {
-  useEffect(() => {
-    const graph = new Graphic({
-      geometry: {
-        type: "point",
-        x: 35,
-        y: 36,
-      },
-      symbol: {
-        type: "simple-marker",
-        color: "red",
-        size: 35,
-      },
-    });
+import Faker from "faker";
 
-    view.graphics.add(graph);
+const MapGraphics = ({ view }) => {
+  Faker.locale = "ar";
+
+  let loactions = [];
+  for (let index = 0; index < 10; index++) {
+    loactions.push({
+      name: Faker.name.findName(),
+      address: Faker.address.streetAddress(),
+      latitude: Faker.address.latitude(),
+      longitude: Faker.address.longitude(),
+    });
+  }
+
+  let loactions2 = [];
+  for (let index = 0; index < 10; index++) {
+    loactions2.push({
+      name: Faker.name.findName(),
+      address: Faker.address.streetAddress(),
+      latitude: Faker.address.latitude(),
+      longitude: Faker.address.longitude(),
+    });
+  }
+  useEffect(() => {
+    const glResult1 = view.map.findLayerById("glResult1");
+
+    if (glResult1) {
+      loactions.map((location) => {
+        const graph = new Graphic({
+          geometry: {
+            type: "point",
+            latitude: location.latitude,
+            longitude: location.longitude,
+          },
+          symbol: {
+            type: "simple-marker",
+            color: "red",
+            size: 14,
+          },
+          popupTemplate: {
+            title: location.name,
+            content: location.address,
+          },
+        });
+        glResult1.add(graph);
+      });
+    }
+
+    const glResult2 = view.map.findLayerById("glResult2");
+
+    if (glResult2) {
+      loactions2.map((location) => {
+        const graph = new Graphic({
+          geometry: {
+            type: "point",
+            latitude: location.latitude,
+            longitude: location.longitude,
+          },
+          symbol: {
+            type: "simple-marker",
+            color: "blue",
+            size: 20,
+          },
+          popupTemplate: {
+            title: location.name,
+            content: location.address,
+          },
+        });
+        glResult2.add(graph);
+      });
+    }
   }, []);
 
   return null;
--- src/MyMap.js
+++ src/MyMap.js
@@ -3,18 +3,28 @@ import MapView from "@arcgis/core/views/MapView";
 import { useEffect, useRef, useState } from "react";
 import MapGraphics from "./MapGraphics";
 import MapWidgets from "./MapWidgets";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
 
 const MyMap = () => {
   const mapRef = useRef(null);
   const [view, setView] = useState(null);
+
+  const glResult1 = new GraphicsLayer({
+    id: "glResult1",
+  });
+
+  const glResult2 = new GraphicsLayer({
+    id: "glResult2",
+  });
 
   useEffect(() => {
     new MapView({
       container: mapRef.current,
       map: new Map({
         basemap: "dark-gray",
+        layers: [glResult1, glResult2],
       }),
-      zoom: 3,
+      // zoom: 3,
     }).when((view) => setView(view));
   }, []);
 
