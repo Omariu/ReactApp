f8083f569486135a871e540692eb214b4fcd250b
Omar Ismail <omariu_dbpro@outlook.com>
2021-11-06 16:52:44 +0300

    hitTest and esri-widget

--- src/MapGraphics.js
+++ src/MapGraphics.js
@@ -1,9 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Graphic from "@arcgis/core/Graphic";
 
 import Faker from "faker";
 
 const MapGraphics = ({ view }) => {
+  const formRef = useRef();
+  const [state, setState] = useState({
+    name: "",
+    address: "",
+  });
   Faker.locale = "ar";
 
   let loactions = [];
@@ -32,7 +37,7 @@ const MapGraphics = ({ view }) => {
             color: "red",
             size: 14,
           },
-          popupTemplate: {
+          attributes: {
             title: location.name,
             content: location.address,
           },
@@ -40,9 +45,32 @@ const MapGraphics = ({ view }) => {
         glResult1.add(graph);
       });
     }
+
+    view.ui.add(formRef.current, "top-right");
+    view.on("click", (event) => {
+      view.hitTest(event).then((resp) => {
+        if (resp.results.length > 0) {
+          setState({
+            name: resp.results[0].graphic.attributes.title,
+            address: resp.results[0].graphic.attributes.content,
+          });
+        }
+      });
+    });
   }, []);
 
-  return null;
+  return (
+    <div
+      ref={formRef}
+      className="esri-widget"
+      style={{ margin: 5, padding: 5 }}
+    >
+      <input type="text" value={state.name} className="esri-input" />
+      <br />
+      <br />
+      <input type="text" value={state.address} className="esri-input" />
+    </div>
+  );
 };
 
 export default MapGraphics;
