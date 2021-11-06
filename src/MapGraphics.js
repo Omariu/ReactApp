cfb655b1d99f6cbb053041af65ae852781f27a4a
Omar Ismail <omariu_dbpro@outlook.com>
2021-11-06 16:08:49 +0300

    MapGraphics

--- src/MapGraphics.js
+++ src/MapGraphics.js
@@ -16,16 +16,6 @@ const MapGraphics = ({ view }) => {
     });
   }
 
-  let loactions2 = [];
-  for (let index = 0; index < 10; index++) {
-    loactions2.push({
-      name: Faker.name.findName(),
-      address: Faker.address.streetAddress(),
-      latitude: Faker.address.latitude(),
-      longitude: Faker.address.longitude(),
-    });
-  }
-
   useEffect(() => {
     const glResult1 = view.map.findLayerById("glResult1");
 
@@ -48,30 +38,6 @@ const MapGraphics = ({ view }) => {
           },
         });
         glResult1.add(graph);
-      });
-    }
-
-    const glResult2 = view.map.findLayerById("glResult2");
-
-    if (glResult2) {
-      loactions2.map((location) => {
-        const graph = new Graphic({
-          geometry: {
-            type: "point",
-            latitude: location.latitude,
-            longitude: location.longitude,
-          },
-          symbol: {
-            type: "simple-marker",
-            color: "blue",
-            size: 20,
-          },
-          popupTemplate: {
-            title: location.name,
-            content: location.address,
-          },
-        });
-        glResult2.add(graph);
       });
     }
   }, []);
